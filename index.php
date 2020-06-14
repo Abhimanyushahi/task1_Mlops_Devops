fc8260becc74fa5fc62ffd093321ce0701a6502b
AbhimanyuShahi <63775603+Abhimanyushahi@users.noreply.github.com>
2020-06-14 22:43:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-welcome in my website
-task1_mlops
-hello everyone
-how are you
-this from devleloper side [D[D[D[D[D[D[D[D[D[D[[[C[C[C[eloper[C[C[C[C[C[C[C[C[C
-2nd time change the code 
+<?php 
+echo "Welcome To My Home Page!"
+?>
